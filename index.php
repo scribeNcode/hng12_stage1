c43ae538a10747d7a2cce2a6731725236203befe
scribeNcode <deyeye99@gmail.com>
2025-02-03 13:14:02 +0100

    handle user error input

--- index.php
+++ index.php
@@ -4,4 +4,15 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
+// check if user input is  null and if it is not number type in order to handle invalid user input
+if(!isset($_GET['number']) || !is_numeric($_GET['number'])); 
+
+// if user doesnt set any number , set a user input to null and convert array to json
+echo json_encode(['number'=> $_GET['number'] ?? null, 'error' => true, 'message' => 'number is required']);
+
+// send http response code to show the request is invalid
+http_response_code(400);
+
+//stop  execution
+exit;
 ?>
