c1f16389c08fd565aa7791f9e56a83c24c493ea2
scribeNcode <deyeye99@gmail.com>
2025-02-03 14:58:57 +0100

    process request

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
+
+
+
 // check if user input is  null and if it is not number type in order to handle invalid user input
 if(!isset($_GET['number']) || !is_numeric($_GET['number'])); 
 
@@ -15,4 +18,31 @@ http_response_code(400);
 
 //stop  execution
 exit;
+
+
+// convert user input to number 
+$number = intval($_GET['number']);
+
+// process number entered by user 
+$isEven = $number % 2 === 0;  //Checks if it's even or odd.
+$digitSum = array_sum(str_split((string) $number)); //Calculates the sum of its digits.
+
+
+// Fetches a fun fact from http://numbersapi.com.
+$funFact = file_get_contents("http://numbersapi.com/$number/math");
+
+
+
+// Creates a JSON response with all calculated properties and send it back to the frontend.
+echo json_encode([
+  "number" => $number,
+  "is_prime" => isPrime($number),
+  "is_perfect" => isPerfect($number),
+  "properties" => $isEven ? ["even"] : ["odd"],
+  "digit_sum" => $digitSum,
+  "fun_fact" => $funFact
+]);
+
 ?>
+
+
